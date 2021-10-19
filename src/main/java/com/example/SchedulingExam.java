bb73d4bda39611ed227b0d9bc789f1bcc3905865
motomu99 <77305499+motomu99@users.noreply.github.com>
2021-10-19 14:14:26 +0900

    プロパティファイル読み込み

--- src/main/java/com/example/SchedulingExam.java
+++ src/main/java/com/example/SchedulingExam.java
@@ -1,17 +1,20 @@
 package com.example;
 
 import com.example.constants.PropertyValues;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class SchedulingExam {
 
   @Autowired PropertyValues prop;
 
   @Scheduled(cron = "*/1 * * * * *")
   public void execute() {
-    System.out.println(prop.getNames());
+    log.info(prop.getData());
+    log.info(prop.getNames().toString());
   }
 }
--- src/main/java/com/example/constants/PropertyValues.java
+++ src/main/java/com/example/constants/PropertyValues.java
@@ -1,14 +1,18 @@
 package com.example.constants;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
 import java.util.List;
 
-@Component
 @ConfigurationProperties(prefix = "foo")
-@Data
+@ConstructorBinding
+@AllArgsConstructor
+@Getter
+@ToString
 public class PropertyValues {
   private final String data;
   private final List<String> names;
