588ce23203eb4bcba802121b1d99641c581dbe01
motomu99 <77305499+motomu99@users.noreply.github.com>
2021-10-22 19:03:11 +0900

    不要ソース削除

--- src/main/java/com/example/Employee.java
+++ src/main/java/com/example/Employee.java
@@ -1,9 +1,22 @@
 package com.example;
 
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvBindByPosition;
 import lombok.Getter;
 import lombok.ToString;
 
 @ToString
+@Getter
 public class Employee {
-  @Getter private String id;
+  @CsvBindByName(column = "社員番号")
+  @CsvBindByPosition(position = 3)
+  private String id;
+
+  @CsvBindByName(column = "名前")
+  @CsvBindByPosition(position = 1)
+  private String name;
+
+  @CsvBindByName(column = "給与")
+  @CsvBindByPosition(position = 2)
+  private Integer salary;
 }
